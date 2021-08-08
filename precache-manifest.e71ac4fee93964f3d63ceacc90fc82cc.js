142bc6b0824dcd76dbe6c36485d4c321da829353
SouvikChoudhury360 <choudhurysouvik2003@gmail.com>
2021-08-08 18:02:03 +0530

    Updates

---
+++ precache-manifest.e71ac4fee93964f3d63ceacc90fc82cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc1c21052fe767792a5e28ef0e03effa",
+    "url": "/PV_Hub/index.html"
+  },
+  {
+    "revision": "9af2cd6ed713b6f85763",
+    "url": "/PV_Hub/static/css/2.80dce9aa.chunk.css"
+  },
+  {
+    "revision": "8d533bd204da2926fb2d",
+    "url": "/PV_Hub/static/css/main.5a8df4d4.chunk.css"
+  },
+  {
+    "revision": "9af2cd6ed713b6f85763",
+    "url": "/PV_Hub/static/js/2.6df23012.chunk.js"
+  },
+  {
+    "revision": "30545a24812bc2557c4d652dede4cf2a",
+    "url": "/PV_Hub/static/js/2.6df23012.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d533bd204da2926fb2d",
+    "url": "/PV_Hub/static/js/main.dccc48e6.chunk.js"
+  },
+  {
+    "revision": "be6c8f17340e9054bfa2",
+    "url": "/PV_Hub/static/js/runtime-main.7c839974.js"
+  }
+]);
